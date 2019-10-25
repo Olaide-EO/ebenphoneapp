68928fd677c1cda11410b808891300f8899eccd5
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-10-25 23:00:50 +0100

    link all routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from  './components/Cart';
+import Default from './components/Default';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+    <Navbar />
+    <Switch>
+    <Route exact path="/" component={ProductList} />
+    <Route path="/details" component={Details} />
+    <Route path="/cart" component={Cart} />
+    <Route  component={Default} />
+    </Switch>
+    </React.Fragment>
   );
 }
 
