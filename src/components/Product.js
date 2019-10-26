fa779e3a1583da358c39fe0957ec8af848f7ed00
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-10-26 10:38:27 +0100

    add css rule to styled component

--- src/components/Product.js
+++ src/components/Product.js
@@ -59,12 +59,12 @@ export default class Product extends Component {
 const ProductWrapper = styled.div`
 .card{
   border-color:transparent;
-  transition: all 1s linear;
+  transition: all 0.5s linear;
 }
  .card-footer {
    background: transparent;
    border-top: transparent;
-   transition: all 1s linear;
+   transition: all 0.5s linear;
  }
  &:hover{
    .card{
@@ -76,4 +76,35 @@ const ProductWrapper = styled.div`
    }
  }
 
+ .img-container{
+   position :relative;
+   overflow: hidden;
+ }
+ .card-img-top{
+   transition: all 0.5s linear;
+ }
+ .img-container:hover .card-img-top{
+   transform: scale(1.2);
+ }
+
+ .cart-btn{
+   position: absolute;
+   bottom: 0;
+   right: 0;
+   padding: 0.2rem 0.4rem;
+   background: var(--lightBlue);
+   border: none;
+   color: var(--mainWhite);
+   font-size: 1.4rem;
+   border-radius: 0.5rem 0 0 0;
+   transform: translate(100%, 100%);
+   transition: all 0.5s linear;
+ }
+.img-container:hover .cart-btn {
+  transform: translate(0,0);
+}
+.cart-btn: hover{
+  color: var(--mainBlue);
+  cursor: pointer;
+}
 `;
