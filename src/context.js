8ec1f281c6d8b12ad2e4e036262d60e7c9c4a477
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-10-27 09:07:30 +0100

    add modal functions and state

--- src/context.js
+++ src/context.js
@@ -9,7 +9,10 @@ class ProductProvider extends Component {
 
     state={
     products:[],
-    detailProduct: detailProduct
+    detailProduct: detailProduct,
+    cart: [],
+    modalOpen: false,
+    modalProduct: detailProduct
     }
     componentDidMount(){
         this.setProducts();
@@ -33,7 +36,7 @@ class ProductProvider extends Component {
 
 
     handleDetail = (id) => {
-    const product = this.getItem();
+    const product = this.getItem(id);
     this.setState(()=>{
         return {detailProduct: product}
     })
@@ -41,15 +44,42 @@ class ProductProvider extends Component {
     }
 
     addToCart = (id) =>{
-        console.log(`hello from add to cart id is ${id}`);
-    }
+     let tempProducts = [...this.state.products]
+    const index = tempProducts.indexOf(this.getItem(id));
+    const product = tempProducts[index];    
+    product.inCart = true;
+    product.count = 1;
+    const price = product.price;
+    product.total = price;
+    this.setState(()=>{
+        return {products: tempProducts, cart: [...this.state.cart,
+             product] };
+    }, () => {console.log(this.state);
+    })
+
+};
+
+openModal = id => {
+   const product = this.getItem(id);
+   this.setState(()=>{
+       return {modalProduct:product, modalOpen: true}
+   })
+
+}
+closeModal = () =>{
+    this.setState(()=>{
+        return {modalOpen: false}
+    })
+}
 
     render() {
         return (
             <ProductContext.Provider value={{
                 ...this.state,
                 handleDetail: this.handleDetail,
-                addToCart:this.addToCart
+                addToCart:this.addToCart,
+                openModal: this.openModal,
+                closeModal: this.closeModal
             }}>
             {this.props.children}
             </ProductContext.Provider>
