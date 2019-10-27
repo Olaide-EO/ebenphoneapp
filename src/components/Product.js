b9e29034004ff5948b6b9750d2aebc63e3fc3276
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-10-27 09:07:51 +0100

    implement modal view

--- src/components/Product.js
+++ src/components/Product.js
@@ -12,9 +12,18 @@ export default class Product extends Component {
         return (
           <ProductWrapper className="col-9 
           mx-auto col-md-6 col-lg-3 my-3">
+
           <div className="card">
+
+          <ProductConsumer>
+          {(value) => (
+            
           <div className="img-container p-5" 
-          onClick={() => console.log('you clicked me on the image container')}>
+               onClick={() => 
+               value.handleDetail(id)
+               }
+               
+          >
           
           <Link to="/details">
             <img src={img} alt="product"
@@ -23,7 +32,9 @@ export default class Product extends Component {
           
           <button className="cart-btn" 
           disabled={inCart?true:false}
-          onClick={()=>{console.log('added to the cart')
+          onClick={()=>{
+          value.addToCart(id);
+          value.openModal(id);
           }}>
           
           {inCart?(
@@ -37,7 +48,9 @@ export default class Product extends Component {
 
           </button>
           </div>
-
+)}
+          
+          </ProductConsumer>
           {/* card footer */}
           <div className="card-footer d-flex 
           justify-content-between">
