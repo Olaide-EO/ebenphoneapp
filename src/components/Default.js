e4fdb14d6a8f063e17d6c83104d18ad111ac7a0c
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-10-28 18:59:08 +0100

    set up default component

--- src/components/Default.js
+++ src/components/Default.js
@@ -2,9 +2,23 @@ import React, { Component } from 'react'
 
 export default class Default extends Component {
     render() {
+        console.log(this.props);
+
         return (
-            <div>
-                <h5>Hello from Dfault</h5>
+            <div className="container">
+            <div className="row">
+            <div className="col-10 mx-auto text-center text-title
+             text-uppercase pt-5">
+              <h1 className="display-3">404</h1>
+              <h1>error</h1>
+              <h3>page not found</h3>
+              <h3>the requested URL<span className="text-danger">
+              {this.props.location.pathname}
+              </span>{" "}
+              was not found</h3>
+             </div>
+            </div>
+
             </div>
         )
     }
