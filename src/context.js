e3bf5b431f34757c3358c8476e8b37d4ac86e4be
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-10-28 18:18:21 +0100

    add cart handlers

--- src/context.js
+++ src/context.js
@@ -12,7 +12,10 @@ class ProductProvider extends Component {
     detailProduct: detailProduct,
     cart: [],
     modalOpen: false,
-    modalProduct: detailProduct
+    modalProduct: detailProduct,
+    cartSubTotal:0,
+    cartTax: 0,
+    cartTotal: 0
     }
     componentDidMount(){
         this.setProducts();
@@ -54,7 +57,8 @@ class ProductProvider extends Component {
     this.setState(()=>{
         return {products: tempProducts, cart: [...this.state.cart,
              product] };
-    }, () => {console.log(this.state);
+    }, () => {
+        this.addTotals();
     })
 
 };
@@ -72,6 +76,70 @@ closeModal = () =>{
     })
 }
 
+increment = (id) => {
+  let tempCart = [...this.state.cart];
+  const selectedProduct = tempCart.find(item=> item.id === id);
+   
+  const index = tempCart.indexOf(selectedProduct);
+   const product = tempCart[index];
+
+   product.count = product.count + 1;
+   product.total = product.count * product.price;
+
+   this.setState(()=>{return{cart:[...tempCart]}},()=>{
+       
+   })
+}
+
+decrement = (id) => {
+    console.log('this is decrement method');
+}
+
+removeItem = (id) => {
+ let tempProducts = [...this.state.products];
+ let tempCart = [...this.state.cart];
+ 
+ tempCart = tempCart.filter(item => item.id !== id);
+ 
+ const index = tempProducts.indexOf(this.getItem(id));
+ let removedProduct = tempProducts[index];
+ removedProduct.inCart= false;
+ removedProduct.count = 0;
+ removedProduct.total = 0;
+
+ this.setState(()=>{
+     return {
+         cart: [...tempCart],
+         products: [...tempProducts]
+     }
+ }, ()=> {
+     this.addTotals();
+ })
+}
+
+clearCart = () =>{
+this.setState(()=>{
+    return { cart: [] };
+}, ()=>{
+    this.setProducts();
+    this.addTotals();
+});
+}
+
+addTotals = () =>{
+    let subTotal = 0;
+    this.state.cart.map(item => (subTotal += item.total));
+    const tempTax = subTotal * 0.1;
+    const tax = parseFloat(tempTax.toFixed(2));
+    const total = subTotal + tax;
+    this.setState(()=> {
+        return {
+            cartSubTotal: subTotal,
+            cartTax: tax,
+            cartTotal: total
+        }
+    })
+}
     render() {
         return (
             <ProductContext.Provider value={{
@@ -79,7 +147,11 @@ closeModal = () =>{
                 handleDetail: this.handleDetail,
                 addToCart:this.addToCart,
                 openModal: this.openModal,
-                closeModal: this.closeModal
+                closeModal: this.closeModal,
+                increment: this.increment,
+                decrement: this.decrement,
+                removeItem: this.removeItem,
+                clearCart: this.clearCart
             }}>
             {this.props.children}
             </ProductContext.Provider>
